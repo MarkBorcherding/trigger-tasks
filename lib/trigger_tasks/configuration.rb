69534912abf2934911d17b8780b7c1f8b1d4f644
Mark Borcherding <markborcherding@gmail.com>
2013-01-18 09:42:28 -0600

    using the config in defining the tasks

--- lib/trigger_tasks/configuration.rb
+++ lib/trigger_tasks/configuration.rb
@@ -1,5 +1,7 @@
 require 'hashie'
 
 class Configuration < Hashie::Dash
+  property :forge_path, :required => true, :default => 'forge'
   property :namespace, :required => true, :default => 'forge'
+  property :default_platform, :default => 'ios'
 end
--- lib/trigger_tasks/trigger_tasks.rb
+++ lib/trigger_tasks/trigger_tasks.rb
@@ -5,69 +5,81 @@ class TriggerTasks < ::Rake::TaskLib
 
 
   def initialize
-    config = Configuration.new
-    yield config if block_given?
-    define_tasks config
+    @config = Configuration.new
+    yield @config if block_given?
+    define_tasks
   end
 
 
   private
 
-  def define_tasks config
+  def forge args
+    sh "#{config.forge_path} #{args}"
+  end
 
-    desc 'Open the app in the device'
-    task :device => 'forge:ios:device'
+  def tasks_namespace
+    @config.namespace
+  end
 
-    desc 'Open the app in the simulator'
-    task :simlator => 'forge:ios:simulator'
+  def define_tasks
 
-    desc 'Open the app in the iOS simulator'
-    task :forge => 'forge:ios'
+    if @config.default_platform
+      platform = @config.default_platform
+      desc 'Open the app in the device'
+      task :device => "#{tasks_namespace}:#{platform}:device"
 
-    namespace config.namespace do
+      desc 'Open the app in the simulator'
+      task :simlator => "#{tasks_namespace}:#{platform}:simulator"
 
       desc 'Open the app in the iOS simulator'
-      task :ios  => %w{ forge:ios:run }
+      task tasks_namespace => "#{tasks_namespace}:ios"
+    end
+
+
+    namespace tasks_namespace do
+
+      desc 'Open the app in the iOS simulator'
+      task :ios  => %W{ #{tasks_namespace}:ios:run }
 
       namespace :ios do
 
         desc 'Run the app on the device'
-        task :device  => %w{ forge:ios:run:device }
+        task :device  => %W{ #{tasks_namespace}:ios:run:device }
 
         desc 'Build the app for iOS'
         task :build do
-          sh 'forge build ios'
+          forge 'build ios'
         end
 
         desc 'Package the app for iOS deveployment in the simulator or device'
         task :package  => %w{ forge:ios:build }do
-          sh 'forge package ios'
+          forge 'package ios'
         end
 
         desc 'Run the app in the location specified in the local_config.json'
         task :run  => %w{ forge:ios:package } do
-          sh 'forge run ios'
+          forge 'run ios'
         end
 
         namespace :run do
           desc 'Run the app in the simulator'
           task :device => %w{ forge:ios:package } do
-            sh 'forge run ios --ios.device device'
+            forge 'run ios --ios.device device'
           end
 
           desc 'Run the device on the device'
           task :simulator => %w{ forge:ios:package } do
-            sh 'forge run ios --ios.device simulator'
+            forge 'run ios --ios.device simulator'
           end
 
           namespace :simulator do
             desc 'Force the app to run in the iPhone simulator'
             task :iphone  => %w{ forge:ios:package } do
-              sh 'forge run ios --ios.device simulator --ios.simulatorfamily iphone'
+              forge 'run ios --ios.device simulator --ios.simulatorfamily iphone'
             end
             desc 'Force the app to run in the iPad simulator'
             task :ipad  => %w{ forge:ios:package } do
-              sh 'forge run ios --ios.device simulator --ios.simulatorfamily ipad'
+              forge 'run ios --ios.device simulator --ios.simulatorfamily ipad'
             end
           end
         end
